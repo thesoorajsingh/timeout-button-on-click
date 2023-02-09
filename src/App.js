ddcf0d03c38736edd6ad49aa8569ef00aa66b6a2
Sooraj M Singh <soorajsingh1999@gmail.com>
2023-02-09 12:14:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./styles.css";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+
+export default function App() {
+  const [disabled, setDisabled] = useState(false);
+
+  useEffect(() => {
+    const currentTime = dayjs().unix();
+    const localTime = JSON.parse(localStorage.getItem("storedTime"))?.time;
+    console.log(currentTime, localTime);
+    console.log(dayjs(currentTime).isAfter(localTime));
+    console.log(currentTime - localTime);
+    setDisabled(currentTime - localTime > 86400 ? false : true);
+  }, []);
+
+  const handleClick = () => {
+    localStorage.setItem(
+      "storedTime",
+      JSON.stringify({ time: dayjs().unix() })
+    );
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={handleClick}>Set Time</button>
+
+      <button disabled={disabled}>disabled button</button>
+    </div>
+  );
+}
